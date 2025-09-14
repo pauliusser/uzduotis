85bb0e2bb705b58627f6e1337a7cbefaa2a055b1
pauliusser <pauliusser@gmail.com>
2025-09-14 20:13:58 +0300

    carousel improvement

--- main.js
+++ main.js
@@ -3,13 +3,35 @@ const logoWrapper = document.getElementById("logo-wrapper");
 const mainLogo = document.getElementById("main-logo");
 const carouselRightArrow = document.getElementById("right-arrow");
 const carouselLeftArrow = document.getElementById("left-arrow");
-const carouselContainer = document.querySelector(".carousel-container");
+const carouselBackground = document.getElementById("carusel-background");
+const carouselContainer = document.getElementById("carousel-container");
 const carousel = document.getElementById("carousel");
 const carouselItems = document.querySelectorAll(".carousel-item");
-const carouselItemSize = 600;
+const carouselIndicator = document.getElementById("carousel-indicator");
+const carouselIndicatorSlider = document.getElementById("carousel-indicator-slider");
+const carouselItemSize = 650;
 let itemsOnScreen = Math.ceil(window.innerWidth / carouselItemSize);
 let calculatedSize;
 let carouselPosition = 0;
+const totalItems = 6;
+let padding = window.innerWidth * 0.05;
+carouselBackground.style.paddingLeft = `0 ${padding}px`;
+carouselBackground.style.paddingRight = `0 ${padding}px`;
+carouselIndicatorSlider.style.width = `${(itemsOnScreen / totalItems) * 100}%`;
+
+const setArrowsColor = () => {
+  if (carouselPosition + itemsOnScreen >= totalItems) {
+    carouselRightArrow.style.color = "#515854";
+    carouselLeftArrow.style.color = "#97ac9f";
+  } else if (carouselPosition < 1) {
+    carouselLeftArrow.style.color = "#515854";
+    carouselRightArrow.style.color = "#97ac9f";
+  } else {
+    carouselRightArrow.style.color = "#97ac9f";
+    carouselLeftArrow.style.color = "#97ac9f";
+  }
+};
+setArrowsColor();
 
 window.addEventListener("scroll", () => {
   heroImage.style.transform = `translateY(${window.scrollY * 0.9}px)`;
@@ -18,36 +40,53 @@ window.addEventListener("scroll", () => {
 });
 
 const carouselHandler = () => {
+  if (carouselPosition + itemsOnScreen > totalItems) {
+    carouselPosition = totalItems - itemsOnScreen;
+  } else if (carouselPosition < 0) {
+    carouselPosition = 0;
+  }
+  carousel.style.left = -(calculatedSize + 20) * carouselPosition + "px";
+  setArrowsColor();
+  padding = window.innerWidth * 0.05;
+  carouselBackground.style.paddingLeft = padding + "px";
+  carouselBackground.style.paddingRight = padding + "px";
+  carouselIndicatorSlider.style.marginLeft = `${(carouselPosition / totalItems) * 100}%`;
+};
+carouselHandler();
+
+const resizeHandler = () => {
   const screenWidth = window.innerWidth;
   itemsOnScreen = Math.ceil(screenWidth / carouselItemSize);
-  calculatedSize = (screenWidth - 200) / itemsOnScreen - 20 + 20 / itemsOnScreen;
-
+  calculatedSize = (screenWidth - padding * 2) / itemsOnScreen - 20 + 20 / itemsOnScreen;
   carouselContainer.style.height = calculatedSize + "px";
-
-  carousel.style.left = -(calculatedSize + 20) * carouselPosition + "px";
-
   carouselItems.forEach((item) => {
     item.style.width = calculatedSize + "px";
   });
 };
-
-carouselHandler();
-
-const carouselPositionHandler = () => {};
+resizeHandler();
 
 carouselRightArrow.addEventListener("click", () => {
-  if (carouselPosition + itemsOnScreen < 6) {
-    carouselPosition += 1;
+  if (carouselPosition + itemsOnScreen < totalItems) {
+    carouselPosition += itemsOnScreen;
     carouselHandler();
   }
 });
 carouselLeftArrow.addEventListener("click", () => {
   if (carouselPosition > 0) {
-    carouselPosition -= 1;
+    carouselPosition -= itemsOnScreen;
     carouselHandler();
   }
 });
+carouselIndicator.addEventListener("click", (e) => {
+  const rect = carouselIndicator.getBoundingClientRect();
+  const clickX = e.clientX - rect.left;
+  const segmentClick = 1 - (rect.width - clickX) / rect.width; // 0 to 1
+  carouselPosition = Math.floor((segmentClick * totalItems) / itemsOnScreen) * itemsOnScreen;
+  carouselHandler();
+});
 
 window.addEventListener("resize", () => {
+  resizeHandler();
   carouselHandler();
+  carouselIndicatorSlider.style.width = `${(itemsOnScreen / totalItems) * 100}%`;
 });
